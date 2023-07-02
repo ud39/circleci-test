760ffed3e2e5142557991969fec8125cd936a41c
ud39 <ud-gaming@gmx.de>
2023-07-02 21:22:27 +0200

    This time for real

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
 http.createServer(function (req, res) {
-  res.write('This time for real for real'); 
+  res.write('This time for real for real come on'); 
   res.end(); 
 }).listen(8080, '0.0.0.0');
