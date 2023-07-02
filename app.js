75430f6b20f6f7ce0cf19b2207e80666ebd670ed
ud39 <ud-gaming@gmx.de>
2023-07-02 21:29:40 +0200

    Issue should have been solved (merged Conflict)

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
 http.createServer(function (req, res) {
-  res.write('This time for real for real come on'); 
+  res.write('Now this should work.'); 
   res.end(); 
 }).listen(8080, '0.0.0.0');
